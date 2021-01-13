7d72f8c6dd013cb3b202efcd2d94335d2ae0af75
Saeem03 <Saeem.shanto@northsouth.edu>
2021-01-13 23:28:25 +0600

    ok club page done

--- Project Merged/Club page/main.php
+++ Project Merged/Club page/main.php
@@ -1,5 +1,10 @@
+<?php 
+$ClubID = $_GET['Id'];
+// echo '<script>console.log("'."$ClubID".'") </script>';
+?>
+
 <!DOCTYPE html>
-<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script> -->
 
 <style>
     #main {
@@ -23,20 +28,27 @@
     #main a{
         margin: 15px;
     }
-    #main :hover{
-        background-color: red;
-    }
+
     #main a:hover{
         color: black;
         
     }
 </style>
-<div style="border: 2px inset white; padding:2%;background: #9395db">
-    <img class="logo" src="./images/acm.png" alt="Logo">
-    <div class="contact-logo" id="club-links"></div>
+<div class="container">
+<div class="row" style="background-color: #e8e8e8;">
+    <div class="col" >
+    <div id="addressBoard" style="border: 4px inset white;">
+        <img id="clubLogo" class="logo" src="./images/" alt="Logo">
+        <div class="contact-logo" id="club-links"></div>
+    </div>
+    <h1 id="mainClubShortName" style="color: lightcoral;" ></h1>
+    </div>
+  </div>
+    
+    
 </div>
 <div style="padding:20px;padding-top:30px;">
-    <h1 id="mainClubShortName" style="color: lightcoral;" ></h1>
+    <h1 id="mainClubLongName" style="color: lightcoral;" ></h1>
     <p id="mainHeading" style="font-size: large;" ></p>
     <img id="mainHeadImg" src="./images/friends-week.jpg" alt="Club HOME column missing comment" style="width: 90%; height:400px; margin:20px">
     <hr>
@@ -44,19 +56,23 @@
 <script>
     
 $(document).ready( function() {
+    var id = "<?php echo $ClubID; ?>";
+    console.log(id);
             $.ajax({
-                type: 'POST',
+                type: 'post',
                 url: './database/clubdescription.php',
-                data: 'id=testdata',
+                data: ({'id': id }),
                 dataType: 'json',
                 cache: false,
                 success: function(result) {
-                    // console.log("main",result);
+                    console.log("main",result);
                     designMain(result);
                 },
-                error: function(){
-        window.alert("Wrong query 'queryDB.php': " + query);
-                }
+                error: function(e){
+        console.log("Something wrong with ./database/clubdescription.php': ");
+        console.log(e);
+                },
+               
             });
         });
 
@@ -64,9 +80,14 @@ $(document).ready( function() {
     function designMain(mainArray){
         var strID=0;
         $('#mainClubShortName').append(mainArray[0][0]['Club_Name']  );
+        $('#mainClubLongName').append(mainArray[0][0]['Club_Fname']  );
         $('#mainHeading').append(mainArray[0][0]['Description']  );
-        $('#mainHeadImg').attr('src','./images/'+ mainArray[0][0]['ClubDP']);
+        $('#mainHeadImg').attr('src','../images/'+ mainArray[0][0]['ClubDP']);
+        $('#clubLogo').attr('src','../images/'+ mainArray[0][0]['Club_Logo']);
         // alert('./images/'+ mainArray[0][0]['Club_Name']+'Head.jpg');
+        if(mainArray[1].length==0){
+            $("#addressBoard").hide();
+        }
         for(var i=0;i<mainArray[1].length;i++)
         {
             if(/facebook/.test(mainArray[1][i]["link"].toLowerCase()))
@@ -76,5 +97,6 @@ $(document).ready( function() {
             else
                 $("#club-links").append('<a href=https://www.'+ mainArray[1][i]["link"] +'><i class="fa fa-link" aria-hidden="true"></i></a>');
         }
+        
     }
 </script>
